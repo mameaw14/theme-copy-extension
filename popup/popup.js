1ea9a617f5e6ee78a98fd8816728d3ec2121e532
mameaw14 <pinyapornster@gmail.com>
2019-04-03 18:00:31 +0700

    restructure project

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,3 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 "use strict";
 
 let buildColor = document.getElementById("buildColor");
@@ -9,11 +5,11 @@ let changeColor = document.getElementById("changeColor");
 
 buildColor.onclick = function (element) {
   chrome.tabs.executeScript({
-    file: './buildTheme.js'
+    file: '../js/buildTheme.js'
   })
 };
 changeColor.onclick = function(element) {
   chrome.tabs.executeScript({
-    file: './applyTheme.js'
+    file: '../js/applyTheme.js'
   })
 };
